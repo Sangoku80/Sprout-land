9fe97057fdcec5a68e47414b6a4e547932188896
Sangoku80 <121511163+Sangoku80@users.noreply.github.com>
2023-04-27 22:50:56 +0200

    Add files via upload

---
+++ Sprout land/code/main.py
@@ -0,0 +1,35 @@
+import pygame
+from settings import *
+from level import Level
+import sys
+
+
+class Game:
+
+    def __init__(self):
+        pygame.init()
+
+        # create the window
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption("Sprout land")
+
+        # clock
+        self.clock = pygame.time.Clock()
+
+        self.level = Level()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            dt = self.clock.tick() / 1000
+            self.level.run(dt)
+            pygame.display.update()
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
